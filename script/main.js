b257dad5201077c9943cb4aa229f54adae19691b
Pammachius Jérôme <scipions.prof@free.fr>
2023-11-29 14:13:33 +0100

    exo fini

--- script/main.js
+++ script/main.js
@@ -1,21 +1,26 @@
 // déclaration des variables global
 let saisie;
-
+let message = document.getElementById("message");
 
 
 function verificationEmail() {
     saisie = document.getElementById("email").value;
+    if (event.key === "Enter") {
 
-    let pattern = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}$/i;
- 
-    if (pattern.test(saisie))
-    {
-        window.alert('La saisie est une adresse email valide !');
-    }
-    else
-    {
-        window.alert('La saisie est invalide !');
+        let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        
+        if (pattern.test(saisie))
+        {
+            message.innerHTML = '<span class="text-success">La saisie est une adresse email valide !</span>';
+            // window.alert('La saisie est une adresse email valide !');
+        }
+        else
+        {
+            message.innerHTML = '<span class="text-danger">La saisie est invalide !</span>';
+            // window.alert('La saisie est invalide !');
+        }
     }
 }
 
-document.getElementById("email").addEventListener("keydown", verificationEmail, false);
+document.getElementById("email").addEventListener("keypress", verificationEmail, false);
+
